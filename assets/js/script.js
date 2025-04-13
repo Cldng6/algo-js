0e0131407fc667894f2dc3c1ea67ef09795d9411
Cldng6 <claudengundoli@gmail.com>
2025-04-13 22:04:36 +0200

    declaration  variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 // Script Js
+// trier un tableau avec l'algo du tri à bulles
+let nombres = [5, 1, 8, 3, 2]
+
+function triABulles(nombres) {
+    for (let index = 0; index < nombres.length; index++) {
+        const element = array[index];
+        return console.log();
+        
+    }
+}
