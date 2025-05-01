3d35d55a2ece0c588214cbe7529ee13f0a4da27c
Cldng6 <claudengundoli@gmail.com>
2025-05-01 11:02:53 +0200

    correction!

--- assets/js/script-tri.js
+++ assets/js/script-tri.js
@@ -3,17 +3,23 @@ let resultat = triABulles(nombres);
 console.log("Tableau trié :", resultat); // [1, 2, 3, 5, 8]
 
 
-
 function triABulles(nombres) {
-    for (let i = 0; i < nombres.length; i++) {
-        for (let j = 0; j < nombres.length - 1 - i; j++) {
+    // varible index = 0; si i est superier à nombres de boucle dont length et long
+    // boucle continue
+    for (let index = 0; index < nombres.length; index++) {
+        // Variable temps = faux
+        let temps = false;
+        // varible j = 0; si j est superier à nombres de boucle dont length est eleve
+        // remplace l'index----
+        for (let j = 0; j < nombres.length - 1 - index; j++) {
             if (nombres[j] > nombres[j + 1]) {
                 // echange des valeurs
-                let temp = nombres[j];
-                nombres[j] = nombres[j + 1] = nombres[j] = nombres[j + 1], temp[j];
-                nombres[j + 1] = temp;
+                [nombres[j] = nombres[j + 1] = nombres[j + 1], nombres[j]];
+                temps = true
+                console.log(temps);
             }
         }
+
     }
     return nombres; // retourne le tableau trié à la fin
 }
