29acbc1d9900c98ff0e385726a93ef3f52da7235
Cldng6 <claudengundoli@gmail.com>
2025-05-02 10:46:09 +0200

    create function

--- assets/js/script.calculator.js
+++ assets/js/script.calculator.js
@@ -1,4 +1,39 @@
+// let firstNumber = prompt('Entrez le premier nombre :')
+// let secondNumber = prompt('Entrez le second nombre :')
+// let continuer = true;
 
-function operation(addition, soustration, multiplication, division ) {
-
+function operation() {
+    const menu = ("Choisissez l'operation = Addition : 1, Soustraction : 2, multilication : 3, division : 4, Quitter : 5")
+    
+    while ([1,2,3,4,5,6]) {
+        console
+    }
 }
+
+
+
+// const addition = 1
+// const soustration = 2
+// const multiplication = 3
+// const division = 4
+// const exit = 5
+// const addOperation = operation.prompt('')
+
+// switch (operation) {
+//     case '1':
+//      '+';
+//     case '2':
+//      '-';
+//     case '3':
+//      '*';
+//     case '4':
+//      '/';
+//     case '5':
+//     console.log('quiter');
+    
+//     break
+//     default:
+//         continuer
+//         console.log('Résultat :', firstNumber, addOperation, secondNumber);
+// }
+
